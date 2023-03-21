2c9f394c37a960872d48cc99677c79cd0461ccc3
DALM1 <92697916+DALM1@users.noreply.github.com>
2023-03-22 00:07:05 +0100

    Updates serv.py

--- client2.py
+++ client2.py
@@ -1,5 +1,4 @@
 import socket
-import threading
 
 host = "localhost"
 port = 5000
@@ -7,33 +6,27 @@ port = 5000
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((host, port))
 
-# Demander le mot de passe
-password = input("Entrez le mot de passe pour accéder au chat: ")
-
-# Envoyer le mot de passe au serveur
+password = input("Entrez le mot de passe: ")
 client_socket.send(password.encode())
 
-# Attendre la réponse du serveur
 response = client_socket.recv(1024).decode()
 
-if response == "OK":
-    print("Mot de passe correct. Vous pouvez maintenant discuter.\n")
-else:
-    print("Mot de passe incorrect. La connexion est fermée.")
+if response == "Mot de passe incorrect":
+    print(response)
     client_socket.close()
     exit()
 
 username = input("Entrez votre nom d'utilisateur: ")
 client_socket.send(username.encode())
 
-def receive_messages():
-    while True:
-        message = client_socket.recv(1024).decode()
-        print(message)
-
-receive_thread = threading.Thread(target=receive_messages)
-receive_thread.start()
-
 while True:
-    message = input()
+    message = input("> ")
     client_socket.send(message.encode())
+
+    response = client_socket.recv(1024).decode()
+    print(response)
+
+    if message.lower() == "/quit":
+        break
+
+client_socket.close()
--- server2.py
+++ server2.py
@@ -3,12 +3,10 @@ import threading
 
 host = "localhost"
 port = 5000
-password = "my_secret_password"  # définir un mot de passe
+password = "48714871"  # définir un mot de passe
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 server_socket.bind((host, port))
-
 server_socket.listen()
 
 clients = []
@@ -19,40 +17,29 @@ def broadcast_message(sender_socket, message):
         if client_socket != sender_socket and client_socket in users:
             client_socket.send(message)
 
-
-
 def handle_client_connection(client_socket):
-    # Demander le mot de passe du client
-    client_socket.send("Entrez le mot de passe: ".encode())
-    password = client_socket.recv(1024).decode()
+    try:
+        # Demander le mot de passe du client
+        client_socket.send("Entrez le mot de passe: ".encode())
+        password = client_socket.recv(1024).decode()
 
-    # Vérifier si le mot de passe est correct
-    if password != password:
-        client_socket.send("Mot de passe incorrect".encode())
-        client_socket.close()
-        return
+        # Vérifier si le mot de passe est correct
+        if password != password:
+            client_socket.send("Mot de passe incorrect".encode())
+            client_socket.close()
+            return
 
+        # Demander le nom d'utilisateur du client
+        client_socket.send("Entrez votre nom d'utilisateur: ".encode())
+        username = client_socket.recv(1024).decode()
 
+        users[client_socket] = username
+        clients.append(client_socket)
 
+        welcome_message = f"{username} a rejoint le chat".encode()
+        broadcast_message(client_socket, welcome_message)
 
-
-
-
-
-
-
-    # Demander le nom d'utilisateur du client
-    client_socket.send("Entrez votre nom d'utilisateur: ".encode())
-    username = client_socket.recv(1024).decode()
-
-    users[client_socket] = username
-    clients.append(client_socket)
-
-    welcome_message = f"{username} a rejoint le chat".encode()
-    broadcast_message(client_socket, welcome_message)
-
-    while True:
-        try:
+        while True:
             message = client_socket.recv(1024)
             if message:
                 broadcast_message(client_socket, f"{username}: {message}".encode())
@@ -65,41 +52,34 @@ def handle_client_connection(client_socket):
                     del users[client_socket]
                 client_socket.close()
                 break
-        except:
-            if client_socket in clients:
-                clients.remove(client_socket)
-            if client_socket in users:
-                username = users[client_socket]
-                broadcast_message(client_socket, f"{username} a quitté le chat".encode())
-                del users[client_socket]
-            client_socket.close()
-            break
-
-        except:
-            # En cas d'erreur, la connexion est fermée proprement
-            if client_socket in clients:
-                clients.remove(client_socket)
-            if client_socket in users:
-                username = users[client_socket]
-                broadcast_message(client_socket, f"{username} a quitté le chat".encode())
-                del users[client_socket]
-            client_socket.close()
-            break
-
+    except KeyboardInterrupt:
+        # En cas d'erreur, la connexion est fermée proprement
+        if client_socket in clients:
+            clients.remove(client_socket)
+        if client_socket in users:
+            username = users[client_socket]
+            broadcast_message(client_socket, f"{username} a quitté le chat".encode())
+            del users[client_socket]
+        client_socket.close()
+    except:
+        # En cas d'erreur, la connexion est fermée proprement
+        if client_socket in clients:
+            clients.remove(client_socket)
+        if client_socket in users:
+            username = users[client_socket]
+            broadcast_message(client_socket, f"{username} a quitté le chat".encode())
+            del users[client_socket]
+        client_socket.close()
 
 threads = []
 
 try:
     while True:
-       
         client_socket, address = server_socket.accept()
         print(f"Connexion acceptée de {address[0]}:{address[1]}")
-
-        
         thread = threading.Thread(target=handle_client_connection, args=(client_socket,))
         threads.append(thread)
         thread.start()
-
 
 except KeyboardInterrupt:
     print("Arrêt demandé, fermeture des connexions...")
@@ -114,12 +94,3 @@ except KeyboardInterrupt:
 
     # Fermer le socket serveur
     server_socket.close()
-
-
-
-
-
-
-
-
-    
